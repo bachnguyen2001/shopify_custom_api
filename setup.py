9ceec4124e504ca674371860fa748f9e7791e3d5
bachnguyen2001 <bachnguyenfptu@gmail.com>
2024-05-21 15:15:06 +0700

    auto update pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup, find_packages
 
-readme = open("README.md").read()
-packages=find_packages(),
-      
+# Read the long description from the README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# Verify packages found
+packages = find_packages()
+print("Packages found:", packages)
+
 setup(
     name="customShopifyAPI",
     version="0.0.1",
-    description="An simple library that help call shopify's API" ,
-    long_description=readme,
+    description="A simple library that helps call Shopify's API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/bachnguyen2001/shopify_custom_api.git",
     author="Nguyen Ngoc Bach",
     author_email="bachnguyenfptu@gmail.com",
@@ -18,10 +24,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-     python_requires='>=3.10',
-     install_requires=[
+    python_requires='>=3.10',
+    install_requires=[
         "requests",
-        "json",
-     ],
-     keywords='shopify rest api'
+    ],
+    keywords='shopify rest api',
 )
