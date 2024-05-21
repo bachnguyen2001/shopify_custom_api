38b2d394fd0d97ef9549cf574971fc90da8d452d
bachnguyen2001 <bachnguyenfptu@gmail.com>
2024-05-21 14:05:46 +0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+readme = open("README.md").read()
+packages=find_packages(),
+      
+setup(
+    name="customShopifyAPI",
+    version="0.0.1",
+    description="An simple library that help call shopify's API" ,
+    long_description=readme,
+    url="http://contoso.com",
+    author="Nguyen Ngoc Bach",
+    author_email="bachnguyenfptu@gmail.com",
+    license="MIT",
+    packages=packages,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+     python_requires='>=3.10',
+     install_requires=[
+        "requests",
+        "json",
+     ],
+     keywords='shopify rest api'
+)
